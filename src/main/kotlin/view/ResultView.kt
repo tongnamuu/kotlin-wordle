3ac30b329e80583a16f20592a9e92e716505fe6c
tongnamuu <thban33@naver.com>
2026-01-19 19:43:45 +0900

    출력시 result 누적하게 변경

--- src/main/kotlin/view/ResultView.kt
+++ src/main/kotlin/view/ResultView.kt
@@ -4,9 +4,12 @@ import domain.vo.GameResult
 
 class ResultView {
 
+    val gameResults = mutableListOf<String>()
+
     fun printResult(gameResult: GameResult) {
         val resultString = gameResult.emojiArray.joinToString("")
-        println(resultString)
+        this.gameResults.add(resultString)
+        gameResults.map { println(it) }
         if (gameResult.isSuccess) {
             println("성공입니다!")
         }
